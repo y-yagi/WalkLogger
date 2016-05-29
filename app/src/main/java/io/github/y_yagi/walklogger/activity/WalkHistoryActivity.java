61a8bf8c4044e52c80d5571da89802583300cac8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-05-29 09:20:52 +0900

    add toolbar to gps log history card

--- app/src/main/java/io/github/y_yagi/walklogger/activity/WalkHistoryActivity.java
+++ app/src/main/java/io/github/y_yagi/walklogger/activity/WalkHistoryActivity.java
@@ -1,9 +1,11 @@
 package io.github.y_yagi.walklogger.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +16,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import com.afollestad.materialdialogs.AlertDialogWrapper;
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import io.github.y_yagi.walklogger.R;
 import io.github.y_yagi.walklogger.databinding.CardWalkBinding;
@@ -73,7 +79,6 @@ public class WalkHistoryActivity extends AppCompatActivity
             startActivity(intent);
             return true;
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
@@ -87,16 +92,65 @@ public class WalkHistoryActivity extends AppCompatActivity
             CardWalkBinding binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.card_walk, layout, false);
             binding.setWalk(walk);
             cardView = (CardView) binding.getRoot();
-
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String uuid = v.findViewById(R.id.name).getTag().toString();
-                    MapsActivity.startActivity(mActivity, uuid);
-                }
-            });
+            setupToolbar(cardView, walk.getUuid());
             layout.addView(cardView);
         }
+    }
+
+    private void setupToolbar(CardView cardView, final String uuid) {
+        Toolbar toolbar = (Toolbar) cardView.findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_walk_history_card);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.item_map) {
+                    MapsActivity.startActivity(mActivity, uuid);
+                } else if (item.getItemId() == R.id.item_update) {
+                    Walk walk = getWalk(uuid);
+                    showUpdateDialog(walk);
+                } else if (item.getItemId() == R.id.item_delete) {
+                    Walk walk = getWalk(uuid);
+                    showDeleteDialog(walk);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void showUpdateDialog(final Walk walk) {
+        new MaterialDialog.Builder(this)
+            .title(R.string.update_walk_dialog_title)
+            .positiveText(R.string.ok)
+            .negativeText(R.string.cancel)
+            .input("", walk.getName(), false, new MaterialDialog.InputCallback() {
+                @Override
+                public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
+                    String walkName = input.toString();
+                    Realm realm = Realm.getDefaultInstance();
+                    realm.beginTransaction();
+                    walk.setName(walkName);
+                    realm.commitTransaction();
+                }
+            })
+            .show();
+    }
+
+    private void showDeleteDialog(final Walk walk) {
+        new MaterialDialog.Builder(this)
+                .title(R.string.delete_walk_dialog_title)
+                .positiveText(R.string.ok)
+                .negativeText(R.string.cancel)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Realm realm = Realm.getDefaultInstance();
+                        realm.beginTransaction();
+                        walk.gpsLogs.deleteAllFromRealm();
+                        walk.deleteFromRealm();
+                        realm.commitTransaction();
+                    }
+                })
+                .show();
     }
 
     private RealmResults<Walk> getWalks() {
@@ -104,5 +158,10 @@ public class WalkHistoryActivity extends AppCompatActivity
         RealmResults<Walk> walks = realm.where(Walk.class)
                 .findAllSorted("start", Sort.DESCENDING);
         return walks;
+    }
+
+    private Walk getWalk(String uuid) {
+        Realm realm = Realm.getDefaultInstance();
+        return realm.where(Walk.class).equalTo("uuid", uuid).findFirst();
     }
 }
